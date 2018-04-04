de4aee8e55ba9f331f32b3cfc89ce84663895e2a
Ursula Laa <uschi.laa@gmx.at>
2018-04-04 16:52:39 +1000

    Added todo list

--- callTour.py
+++ callTour.py
@@ -31,22 +31,19 @@ try:
 	selCols = [int(x) for x in params.get("options","showCols").split(",")]
 	selCols = robjects.IntVector(selCols)
 	dfShow = df.rx(selCols)
+#if this fails we show all columns
+#FIXME should not be df but only numeric columns
 except: dfShow = df
 
 #get color vector
 colN = params.get("style","col")
 if colN in df.names:
-	colV = df.rx2(colN) #first extract column from dataframe
-	colV = base.as_integer(base.factor(colV)) #some reformatting so we get values 1, 2, ...
-	nCol = base.length(base.unique(colV)) #number of colors we need to select from palette
-	pal = colorspace.rainbow_hcl(nCol) #defining color palette
-	col = pal.rx(colV) #defining color vector
-else: col = "black"
+	colV = df.rx2(colN)				#first extract column from dataframe
+	colV = base.as_integer(base.factor(colV))	#some reformatting so we get values 1, 2, ...
+	nCol = base.length(base.unique(colV))		#number of colors we need to select from palette
+	pal = colorspace.rainbow_hcl(nCol)		#defining color palette
+	col = pal.rx(colV)				#defining color vector
+else: col = "black"					#if colors not set by user keep default
 
 tourr.animate_xy(dfShow, col=col)
 
-
-#do more fancy stuff as
-#getting column from dataframe: df.rx("colName") #want to use this e.g. for color, point style
-#select subset from df: selectCols = robjects.StrVector(("rn1", "rn2")) #just give vector of column names
-#then define new df simply as: subDf = df.rx(selectCols)
